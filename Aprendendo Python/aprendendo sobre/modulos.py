6657a49549adf7738d5c4bbb04293b0ef16f2f7c
Nathan Duarte Aguilar <nathanduarteaguilar@gmail.com>
2024-01-28 18:44:42 -0300

    update

---
+++ Aprendendo Python/aprendendo sobre/modulos.py
@@ -0,0 +1,48 @@
+##################################################################################################################################################
+print('-------------------------------------------------------------------------------------------------------------------------------------\n\n')
+##################################################################################################################################################
+
+###### importando biblioteca  math  inteira ######
+
+#import math
+
+#num = int(input("Digite um número: "))
+
+#raiz = math.sqrt(num)
+
+#arredondando para cima usando ceil
+
+#print("A raiz de {} é igual a {}".format(num, math.ceil(raiz)))
+
+#arredondando para baico usando floor
+#print("A raiz de {} é igual a {}".format(num, math.floor(raiz)))
+
+###### importando funções especificas da biblioteca math ######
+
+#from math import sqrt
+
+#num = int(input("Digite um número: "))
+
+#raiz = sqrt(num)
+
+#print("A raiz de {} é igual a {}".format(num, raiz))
+
+###### biblioteca random ######
+
+#import random 
+
+#num = random.randint(1,10)
+
+#print(num)
+
+###### biblioteca emoji ######
+
+import emoji
+
+print(emoji.emojize('Python é :woman:'))
+
+print(emoji.emojize("Olá, mundo :👩‍🍼:"))
+
+##################################################################################################################################################
+print('\n\n-------------------------------------------------------------------------------------------------------------------------------------')
+##################################################################################################################################################
