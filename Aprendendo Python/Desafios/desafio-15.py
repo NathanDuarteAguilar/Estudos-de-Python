50c11de71b7e9aed1f8eee225c22c32e55159c76
Nathan Duarte Aguilar <nathanduarteaguilar@gmail.com>
2024-01-29 01:34:07 -0300

    update

--- Aprendendo Python/Desafios/desafio-15.py
+++ Aprendendo Python/Desafios/desafio-15.py
@@ -2,7 +2,15 @@
 print('-------------------------------------------------------------------------------------------------------------------------------------\n\n')
 ##################################################################################################################################################
 
+# Escreva um programa que pergunte a quantidade de Km percorridos por um carro alugado e a quantidade de dias pelos quais ele foi alugad. Calcule
+# o preço a pagar, sabendo que o carro custa R$60,00 por dia e R$0,15 por km rodado
 
+dias = int(input("Quantos dias alugados ? "))
+km = int(input("Quantos Km rodados ? "))
+
+total =  (dias*60)+(km*0.15)
+
+print("O total a pagar é de R${}".format(total))
 
 ##################################################################################################################################################
 print('\n\n-------------------------------------------------------------------------------------------------------------------------------------')
